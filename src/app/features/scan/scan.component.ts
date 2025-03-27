a2ddf51723854baeef4b035558c0f40d5c65cf4b
fperez15 <francis.perez1590@gmail.com>
2025-03-26 19:22:17 -0500

    Creacion de componentes scan y chat

---
+++ src/app/features/scan/scan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scan',
+  standalone: true,
+  imports: [],
+  templateUrl: './scan.component.html',
+  styleUrl: './scan.component.scss'
+})
+export class ScanComponent {
+
+}
