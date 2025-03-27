8220d0a7fa4143ca8e6c40b438e22fb97e869857
fperez15 <francis.perez1590@gmail.com>
2025-03-27 12:30:56 -0500

    componente chat

--- src/app/features/cards/cards-container/cards-container.component.ts
+++ src/app/features/cards/cards-container/cards-container.component.ts
@@ -12,9 +12,9 @@ import { CommonModule } from '@angular/common';
 export class CardsContainerComponent {
 
   cards = [
-    { icon: 'assets/icons/upload.svg', title: 'Verificar un posible fraude', subtitle: 'Subiendo una captura de pantalla.' },
-    { icon: 'assets/icons/copy.svg', title: 'Analizar mensaje sospechoso', subtitle: 'Pega o escribe el texto a verificar.' },
-    { icon: 'assets/icons/alert.svg', title: 'Reportar mensaje fraudulento', subtitle: 'Al buzon de alertas del BCP.' },
-    { icon: 'assets/icons/safe.svg', title: 'Aprender tips de seguridad', subtitle: 'Y evitar posibles fraudes financieros.' }
+    { id: 1, icon: 'assets/icons/upload.svg', title: 'Verificar un posible fraude', subtitle: 'Subiendo una captura de pantalla.' },
+    { id: 2, icon: 'assets/icons/copy.svg', title: 'Analizar mensaje sospechoso', subtitle: 'Pega o escribe el texto a verificar.' },
+    { id: 3,icon: 'assets/icons/alert.svg', title: 'Reportar mensaje fraudulento', subtitle: 'Al buzon de alertas del BCP.' },
+    { id: 4, icon: 'assets/icons/safe.svg', title: 'Aprender tips de seguridad', subtitle: 'Y evitar posibles fraudes financieros.' }
   ];
 }
