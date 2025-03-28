bfa289891c069de82b946e85c107f73f3b778d83
fperez15 <francis.perez1590@gmail.com>
2025-03-28 09:12:44 -0500

    agregar imagen en el bot

--- src/app/features/chat/chat.component.ts
+++ src/app/features/chat/chat.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { CardsContainerComponent } from "../cards/cards-container/cards-container.component";
 import { ChatServiceService } from '../../services/chat-service.service';
 import { LinkifyPipe } from '../../pipes/linkify.pipe';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-chat',
@@ -13,7 +14,7 @@ import { LinkifyPipe } from '../../pipes/linkify.pipe';
 })
 export class ChatComponent implements OnInit {
 
-  messages: { text: string, sender: 'bot' | 'user' }[] = [];
+  messages: { text: string | SafeHtml, sender: 'bot' | 'user' }[] = [];
 
   constructor(private chatService: ChatServiceService) {}
 
@@ -21,5 +22,9 @@ export class ChatComponent implements OnInit {
     this.chatService.messages$.subscribe(messages => {
       this.messages = messages;
     });
+  }
+
+isString(val: any): boolean {
+  return typeof val === 'string';
   }
 }
--- src/app/pipes/linkify.pipe.ts
+++ src/app/pipes/linkify.pipe.ts
@@ -9,16 +9,22 @@ export class LinkifyPipe implements PipeTransform {
 
   constructor(private sanitizer: DomSanitizer) {}
 
-  transform(text: string): SafeHtml {
-    if (!text) return text;
+  transform(text: string | SafeHtml): SafeHtml {
 
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
-    const withLinks = text.replace(urlRegex, url => {
-      return `<a href="${url}" target="_blank" class="chat-link">${url}</a>`;
-    });
+    if (text && typeof text !== 'string') {
+      return text as SafeHtml;
+    }
 
-    const withLineBreaks = withLinks.replace(/\n/g, '<br>');
+    if (typeof text === 'string') {
+      if (!text) return this.sanitizer.bypassSecurityTrustHtml(''); // Si es vacío, retornamos un SafeHtml vacío
+      const urlRegex = /(https?:\/\/[^\s]+)/g;
+      const withLinks = text.replace(urlRegex, url => {
+        return `<a href="${url}" target="_blank" class="chat-link">${url}</a>`;
+      });
+      const withLineBreaks = withLinks.replace(/\n/g, '<br>');
+      return this.sanitizer.bypassSecurityTrustHtml(withLineBreaks);
+    }
 
-    return this.sanitizer.bypassSecurityTrustHtml(withLineBreaks);
+    return text;
   }
 }
