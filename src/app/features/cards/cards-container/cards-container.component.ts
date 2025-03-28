d5eaab3a87692d7ffdffe7bfa58be2f34035cddc
fperez15 <francis.perez1590@gmail.com>
2025-03-28 02:45:28 -0500

    ajustando diseño en chat y cards-container

--- src/app/features/cards/cards-container/cards-container.component.ts
+++ src/app/features/cards/cards-container/cards-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CardComponent } from '../../../components/card/card.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -12,8 +12,23 @@ import { FormsModule } from '@angular/forms';
 })
 export class CardsContainerComponent {
 
+  @Output() cardSelected = new EventEmitter<number>();
+
+
   cards = [
-    { icon: 'assets/icons/upload.svg', subtitle: 'Analizar mensaje sospechoso' },
-    { icon: 'assets/icons/security.svg', subtitle: 'Aprender tips de seguridad.' },
+    { id: 1, icon: 'assets/icons/upload.svg', subtitle: 'Analizar mensaje sospechoso' },
+    { id: 2, icon: 'assets/icons/security.svg', subtitle: 'Aprender tips de seguridad.' },
   ];
+
+  isTextareaEnabled = false;
+  showCards = true;
+
+  onCardClick(id: number) {
+    if (id === 1) {
+      this.isTextareaEnabled = true;
+      this.showCards = false;
+      console.log('showCards ahora es:', this.showCards);
+    }
+    this.cardSelected.emit(id);
+  }
 }
--- src/app/features/chat/chat.component.ts
+++ src/app/features/chat/chat.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardsContainerComponent } from "../cards/cards-container/cards-container.component";
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardsContainerComponent],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 export class ChatComponent {
+
+  showCards = true;
+
+  messages: { text: string, sender: 'bot' | 'user' }[] = [];
+
+  handleCardSelection(id: number) {
+
+    if (id === 1) {
+      this.messages.push(
+        { text: 'Analizar un mensaje sospechoso', sender: 'user' },
+        { text: 'Sube tu captura o pega el mensaje que quieres analizar:', sender: 'bot' }
+      );
+    } else if (id === 2) {
+      this.messages.push(
+        { text: 'Aprender tips de seguridad.', sender: 'user' },
+        { text: 'Aquí tienes algunos consejos de seguridad para protegerte de fraudes.', sender: 'bot' }
+      );
+    }
+  }
 
 }
